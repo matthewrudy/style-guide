d4ee263438f9446d0b20b3e10a5ea9fbc4a5356f
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2013-05-15 15:56:49 +0800

    rails 4
    4.0.0.rc1

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -3,30 +3,27 @@ require File.expand_path('../boot', __FILE__)
 # Pick the frameworks you want:
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "active_resource/railtie"
 require "sprockets/railtie"
 
-Bundler.require :default, Rails.env
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env)
 require "style_guide"
 
 module Dummy
   class Application < Rails::Application
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
 
-    # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password]
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
 
-    # Enable escaping HTML in JSON.
-    config.active_support.escape_html_entities_in_json = true
-
-    # Enable the asset pipeline
-    config.assets.enabled = true
-
-    # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.0'
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
 
     config.style_guide.paths << Rails.root.join("app/views/style-guide/**/*")
   end
 end
-
--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'a7d32ad85f850f1fe514b22601c18fa75f61f422f075f412606ea900b3fe1d6ae8dbb8020442b729baccef934b4948e5c0ed23f40087f95312316c83c85bf4fd'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'f9df7ba28fc2364d15508ee636a1c574ce56c6a470d55dd7b01646669112de1d81b2876d348f98a5c85b7d94100f02f49459a48c038e24a5665ea660fa172e83'
